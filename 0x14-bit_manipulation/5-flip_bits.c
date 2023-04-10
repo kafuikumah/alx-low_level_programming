418cf175e5bcec7f4cb83999810f2ae2fa6fda19
Kafui Kumah <kafui.kofi.kumah@gmail.com>
2023-04-10 05:50:17 -0700

    Write a function that returns the number of bits you would need to flip to get from one number to anothe

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,6 +10,7 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	// Calculate the bitwise XOR of n and m to get the bits that are different
+	int c = 0
 	unsigned long int xor = n ^ m;
 	
 	// Count the number of bits that are different
